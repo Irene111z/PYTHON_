8a7d9c6f62f0823de443a7cf86d417c3c2c140b6
Irene111z <Iryna.Zmiyevska@cs.khpi.edu.ua>
2022-06-11 12:00:22 +0300

    Добавьте файлы проекта.

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,15 @@
+
+#start learning Python :)
+# <3
+print("My age:", 18, end=" :)\n")
+print("My age:", 1,8, end=" :)\n")
+print("My age: ", 1,8, sep="", end=" :)\n")
+# C++ pow(4,2) ---> Python 4**2 or pow(4, 2)
+print("4^2 =", 4**2,"\n", "5^3 =", 5**3, end="\n")
+print("10/3 =", 10/3, "\n", "10//3 =", 10//3)
+print("min from 0, 12, 3, -5, 8 =", min(0,12,3,-5,8))
+# max() works similarly
+# abs() ---> module
+print(5/3, end=" = ")
+print(round(5/3))# math rounding
+
